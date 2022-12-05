8fce4d45e48aed2ad8c60a955ee4b0e7450a2957
carey-kwan <careykwan1973@yahoo.com>
2022-12-04 19:02:14 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+//import Car from "./components/Car";
+//import Counter from "./components/Counter";
+//import Timer from "./components/Timer";
+import App from "./components/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<Car />);
+//root.render(<Counter />);
+//root.render(<Timer />);
+root.render(<App />);
